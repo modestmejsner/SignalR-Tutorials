9cb7ba8ca41d18abd5a51e724edff692337efc85
modestmejsner <modestmejsner@gmail.com>
2021-03-10 09:16:24 +0100

    initial commit - SIgnalR is set up

---
+++ CTVM2App/Scripts/SignalR.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    var hub = $.connection.echo;
+    var myHub = $.connection.echo;
+    $.connection.myHub.client.announce = function (message) {
+        $("#welcome-messages").append(message + "<br/>")
+    }
+    $.connection.hub
+        .start()
+        .done(function () {
+            console.log("SignalR is working");
+            $.connection.myHub.server.announce("Connected");
+        });
+});
---
+++ CTVM2App/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(CTVM2App.Startup))]
+
+namespace CTVM2App
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+           // ConfigureAuth(app);
+        }
+    }
+}
