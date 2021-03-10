c69d6c83a3fec674b7b20b4c7d63372231287368
modestmejsner <modestmejsner@gmail.com>
2021-03-10 09:51:53 +0100

    Refactoring

--- CTVM2App/Scripts/SignalR.js
+++ CTVM2App/Scripts/SignalR.js
@@ -2,12 +2,19 @@
     var hub = $.connection.echo;
     var myHub = $.connection.echo;
     $.connection.myHub.client.announce = function (message) {
+        writeToPage(message);
+    }
+    var writeToPage = function (message) {
         $("#welcome-messages").append(message + "<br/>")
     }
     $.connection.hub
         .start()
         .done(function () {
-            console.log("SignalR is working");
+            writeToPage("SignalR is working");
             $.connection.myHub.server.announce("Connected");
-        });
+        }
+    )
+        .fail(function() {
+            writeToPage("Error connecting SingalR");
+        })
 });
